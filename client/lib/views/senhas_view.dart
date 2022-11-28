0c531471539b742ad46a92b2848328310444b2a6
JaiPi <29052214+JaiPi@users.noreply.github.com>
2022-11-28 21:38:40 +0000

    Add date picker timeline

--- client/lib/views/senhas_view.dart
+++ client/lib/views/senhas_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 
 class Senhasview extends StatefulWidget {
   const Senhasview({super.key});
@@ -10,11 +9,65 @@ class Senhasview extends StatefulWidget {
 }
 
 class _SenhasviewState extends State<Senhasview> {
+  DatePickerController _controller = DatePickerController();
+
+  DateTime _selectedValue = DateTime.now();
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("SENHAS"),
-    );
-    ;
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.replay),
+        onPressed: () {
+          _controller.animateToSelection();
+        },
+      ),
+        appBar: AppBar(
+          title: Text("TITULO"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20.0),
+          color: Colors.blueGrey[100],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("You Selected:"),
+              const Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              Text(_selectedValue.toString()),
+              const Padding(
+                padding: EdgeInsets.all(20),
+              ),
+              Container(
+                child: DatePicker(
+                  DateTime.now(),
+                  width: 60,
+                  height: 80,
+                  controller: _controller,
+                  initialSelectedDate: DateTime.now(),
+                  selectionColor: Colors.black,
+                  selectedTextColor: Colors.white,
+                  inactiveDates: [
+                    DateTime.now().add(const Duration(days: 3)),
+                    DateTime.now().add(const Duration(days: 4)),
+                    DateTime.now().add(const Duration(days: 7))
+                  ],
+                  onDateChange: (date) {
+                    // New date selected
+                    setState(() {
+                      _selectedValue = date;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
