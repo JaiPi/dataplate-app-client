8c166647adafae6fc5562579983d58fc703fa46c
JaiPi <29052214+JaiPi@users.noreply.github.com>
2022-11-29 10:15:22 +0000

    Add date picker to home view

--- client/lib/views/home_view.dart
+++ client/lib/views/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:intl/intl.dart';
+
 class Homeview extends StatefulWidget {
   const Homeview({super.key});
 
@@ -8,43 +11,84 @@ class Homeview extends StatefulWidget {
 }
 
 class _HomeviewState extends State<Homeview> {
+  DatePickerController _controller = DatePickerController();
+
+  DateTime _selectedValue = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFFFFF),
-      body: ListView(
+      body: Column(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.blueGrey[100],
             child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Bom Dia",
-                        ),
-                        Text(
-                          "Escolha as suas refeições",
-                        )
-                      ],
-                    ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: const DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(
-                              "assets/images/logo.png",
-                            ),
-                          )),
-                    )
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                DatePicker(
+                  DateTime.now(),
+                  width: 60,
+                  height: 80,
+                  controller: _controller,
+                  initialSelectedDate: DateTime.now(),
+                  selectionColor: Colors.black,
+                  selectedTextColor: Colors.white,
+                  inactiveDates: [
+                    DateTime.now().add(const Duration(days: 3)),
+                    DateTime.now().add(const Duration(days: 4)),
+                    DateTime.now().add(const Duration(days: 7))
                   ],
+                  onDateChange: (date) {
+                    // New date selected
+                    setState(() {
+                      _selectedValue = date;
+                    });
+                  },
+                ),
+                Text(DateFormat('dd MMM yyyy').format(_selectedValue)),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Bom Dia",
+                              ),
+                              Text(
+                                "Escolha as suas refeições",
+                              )
+                            ],
+                          ),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: const DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(
+                                    "assets/images/logo.png",
+                                  ),
+                                )),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
