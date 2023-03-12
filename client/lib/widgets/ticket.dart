5ffbd087319b41f69ae62988af2247f41566bd2f
joaofaustino99 <jmofo@iscte-iul.pt>
2023-03-12 20:13:57 +0000

    add basic card layout

--- client/lib/widgets/ticket.dart
+++ client/lib/widgets/ticket.dart
@@ -3,22 +3,42 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Ticket extends StatelessWidget {
-  const Ticket({super.key});
+  final String titulo;
+  final String subtitulo;
+  final String day;
+  final String month;
+  final String fotopath;
+
+  const Ticket(this.titulo, this.subtitulo, this.day, this.month, this.fotopath,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
-      child: const Card(
+      child: Card(
         color: Colors.white,
         shadowColor: Colors.black,
         elevation: 5,
-        child: SizedBox(
-          width: 300,
-          height: 150,
-          child: Center(
-            child: Text("Ticket"),
-          ),
+        child: Column(
+          children: [
+            Image.asset(
+              fotopath,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 150,
+            ),
+            ListTile(
+              title: Text(titulo),
+              subtitle: Text(subtitulo),
+              trailing: Column(
+                children: [
+                  Text(day, textScaleFactor: 2),
+                  Text(month),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
